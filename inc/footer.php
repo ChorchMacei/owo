a4a11dbdb3bcdc51d47f56ce454e29e3e2b648cd
Chorch <chorch@marketdynamo.com>
2024-12-12 15:49:56 -0300

    w

--- inc/footer.php
+++ inc/footer.php
@@ -209,3 +209,6 @@ style="fill:#FFFFFF;">
   </section>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66df17562688b6d820491c3e" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
+  <a href="https://wa.me/573044476190" target="_blank" title="Whatsapp flotante" class="whatsapp-flotante">
+    <img src="images/whatsapp-flotente.png" alt="Whatsapp flotante">
+  </a>
